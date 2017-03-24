d058faff8f9422b1737d7d55d12869273ece6954
xR86 <9412441+xR86@users.noreply.github.com>
2017-03-24 23:33:29 +0200

    updated cloud hw3

--- FII-year3sem2-CC/hw3/server.js
+++ FII-year3sem2-CC/hw3/server.js
@@ -1,7 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var path = require('path');
+
 var port = process.env.PORT || 3000;
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
