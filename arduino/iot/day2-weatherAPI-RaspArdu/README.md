8d7167be48f8d2332113268904ae50e0a8d1a255
Dan Alexandru <dan.alexandru@info.uaic.ro>
2017-06-29 16:42:57 +0300

    updated raspberry IoT weather exercise

---
+++ arduino/iot/day2-weatherAPI-RaspArdu/README.md
@@ -0,0 +1,18 @@
+# Algo
+a bit of everything
+    
+  
+### Device that on proximity displays the temperature (API) based on 3 intervals
+
+![](schema.png)
+>> Fritzing diagram and Wyliodrin project available
+
+![](wyliodrin1.png)
+
+![](wyliodrin2.png)
+
+
+https://github.com/WiringPi/WiringPi-Python/pull/51
+
+https://github.com/WiringPi/WiringPi-Python/issues/47
+
--- arduino/iot/day2-weatherAPI-RaspArdu/weather.py
+++ arduino/iot/day2-weatherAPI-RaspArdu/weather.py
@@ -2,13 +2,52 @@ import urllib2 as ur2
 import json
 from time import *
 
-from wyliodrin import *
+
+import platform
+
+#part of platform.uname()
+if platform.node() != 'raspberrypi':
+	print('ERROR: You should be on a raspberry to run this.')
+	exit()
+
+if platform.release()[:3] == '4.9':
+	print('\n\nERROR: You will have problems with WiringPi if you have a kernel >= 4.9')
+	print('One possible solution is: https://github.com/WiringPi/WiringPi-Python/pull/51 \n');
+	print('If you want this problem fixed, contact the first 2 contributors:')
+	print('\thttps://github.com/WiringPi/WiringPi-Python/graphs/contributors \n\n');
+	exit()
+
+
+code = -1
+try:
+	from wyliodrin import *
+except ImportError, e:
+	print('\n\nERROR: ', e)
+	print('\twyliodrin not present, trying wiringpi as fallback...')
+	try:
+		from wiringpi import *
+	except ImportError, e:
+		print('\tdid not find either wyliodrin or wiringpi')
+	else:
+		print('\nWiringpi imported succesfully')
+		code = 1
+else:
+	print('\nWyliodrin imported succesfully')
+	code = 0
+finally:
+	print 'Try installing ...'
+
+
 import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-print("Serial opened !")
+print("\nSerial opened !")
 
 def interm_led(led_port):
+
+	# if wiringpi has been imported
+	if code == 1:
+		wiringPiSetupGpio()
 	pinMode (led_port, OUTPUT)
 
 	i = 0
@@ -21,22 +60,26 @@ def interm_led(led_port):
 		i += 1
 	# 1s * repeat = 2s ...
 
+# looping
 while True:
-
 	print("readline: %s" % ser.readline())
 	check = int(ser.readline())
-	sendSignal ("sgndist", check)
+
+	# if wyliodrin has been imported
+	if code == 0:
+		sendSignal ("sgndist", check)
 
 	if check < 100:
-		print("Open API")
-		response = ur2.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Iasi,RO&units=metric&appid=<<>>")
+		print("Distance sensor triggered !")
+		response = ur2.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Iasi,RO&units=metric&appid=63b68e2ec15393a891508a5796f9f1ac")
 		json_response = response.read()#.decode(response.info().get_param('charset') or 'utf-8') #py3
 
 		resp = json.loads(json_response)
 		temp_val = resp["main"]["temp"]
-		print('\nTemperature: %s' % temp_val)
+		print('Temperature: %s' % temp_val)
 
-		sendSignal ("sgntemp", temp_val)
+		if code == 0:
+			sendSignal ("sgntemp", temp_val)
 
 		if temp_val < 10:
 			# open green
@@ -50,5 +93,4 @@ while True:
 			# open red
 			print("Red LED opened !")
 			interm_led(15)
-
 		#sleep ((500)/1000.0)
