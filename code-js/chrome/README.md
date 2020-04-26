a1427352642c8c6ff473864d392bf699c28a33bf
xR86 <dan.alexandru@info.uaic.ro>
2020-04-27 00:05:11 +0300

    moved chrome extension folder

--- code-js/chrome/README.md
+++
@@ -1,6 +0,0 @@
-# Algo
-a bit of everything
-  
-  
-## Chrome API
-Chrome API used in extensions / apps  
--- code-js/chrome/hello-world-ext/background.js
+++
@@ -1,5 +0,0 @@
-// chrome.app.runtime.onLaunched.addListener(function() {
-// });
-chrome.browserAction.onClicked.addListener(function(tab) { 
-	chrome.tabs.create({url: "/page/page.html"});
-});
